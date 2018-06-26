90c2d702ed7d6d9ce4d7e725123da451e602898f
hejiasheng <szhejason@gmail.com>
2018-06-26 19:53:07 +0800

    change ui

--- src/pages/component/pages/camera/camera.js
+++ src/pages/component/pages/camera/camera.js
@@ -5,7 +5,7 @@ import { Camera, View } from '@tarojs/components'
 
 import Header from '../../../../components/head/head'
 
-export default class About extends Component {
+export default class PageView extends Component {
   config = {
     navigationBarTitleText: '相机'
   }
@@ -14,11 +14,19 @@ export default class About extends Component {
     super(...arguments)
   }
 
+  handleError () {
+    alert('您的浏览器不允许使用摄像头')
+  }
+
+  handleStop () {
+    alert('摄像头被非正常终止')
+  }
+
   render () {
     return (
       <View>
         <Header title='Camera'></Header>
-        <Camera></Camera>
+        <Camera onStop={this.handleStop} onError={this.handleError}></Camera>
       </View>
     )
   }
