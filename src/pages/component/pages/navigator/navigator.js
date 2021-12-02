277561898bbb69c5c24c52e0ef5421f2cfcf81b9
huozhongyi <huozhongyi@jd.com>
2021-12-02 17:45:22 +0800

    feat: change radio page

--- src/pages/component/pages/navigator/navigator.js
+++ src/pages/component/pages/navigator/navigator.js
@@ -20,19 +20,29 @@ export default class PageSwitch extends React.Component {
           <View className='components-page__body-example example'>
             <View className='example-body'>
               {
-                Taro.getEnv() != Taro.ENV_TYPE.WEB ? <View className='example-body__navigators'>
-                  <Navigator
-                    url='/pages/component/pages/navigatePage/navigatePage'
-                    className='example-body__navigators-item'>
-                    <View className='example-body__navigator'>跳转到新页面</View>
-                  </Navigator>
-                  <Navigator
-                    url='/pages/component/pages/redirectPage/redirectPage'
-                    className='example-body__navigators-item'
-                    open-type='redirect'>
-                    <View >在当前页打开</View>
-                  </Navigator>
-                </View> : <Text>暂未支持，请使用Taro API</Text>
+                process.env.TARO_ENV != 'h5' ? 
+                  <View className='example-body__navigators'>
+                    <Navigator
+                      url='/pages/component/pages/navigatePage/navigatePage'
+                      className='example-body__navigators-item'
+                      hoverClass='example-body__navigators-item'
+                    >
+                      <View className='example-body__navigator'>
+                        <Text>跳转到新页面</Text>
+                      </View>
+                    </Navigator>
+                    <Navigator
+                      url='/pages/component/pages/redirectPage/redirectPage'
+                      className='example-body__navigators-item'
+                      hoverClass='example-body__navigators-item'
+                      open-type='redirect'
+                    >
+                      <View className='example-body__navigator'>
+                        <Text>在当前页打开</Text>
+                      </View>
+                    </Navigator>
+                  </View> : 
+                  <Text>暂未支持，请使用Taro API</Text>
               }
 
             </View>
